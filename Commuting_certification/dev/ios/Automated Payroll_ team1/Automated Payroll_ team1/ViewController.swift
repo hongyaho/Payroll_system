31ac1e7811eb687b09490215d5ec5143a4233048
Joon <kjo11300@naver.com>
2021-05-20 22:29:51 +0900

    feat : 기본 프로젝트 파일 생성

---
+++ Commuting_certification/dev/ios/Automated Payroll_ team1/Automated Payroll_ team1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Automated Payroll_ team1
+//
+//  Created by Kim JoonOh on 2021/05/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
