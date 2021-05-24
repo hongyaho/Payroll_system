035a11e10e0cec59ab6ee6ab7ba2e3bfb1a64cf0
Joon <kjo11300@naver.com>
2021-05-24 21:49:03 +0900

    feat : 사진촬영 화면 생성 사진촬영 화면 컨트롤러파일 생성

---
+++ Commuting_certification/dev/ios/Automated Payroll_ team1/Automated Payroll_ team1/TakePhoto.swift
@@ -0,0 +1,29 @@
+//
+//  TakePhoto.swift
+//  Automated Payroll_ team1
+//
+//  Created by Kim JoonOh on 2021/05/24.
+//
+
+import UIKit
+
+class TakePhoto: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
