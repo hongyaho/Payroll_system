6454a45b9a8d31afed437b32b9a55050c5b1f60f
Joon <kjo11300@naver.com>
2021-05-29 01:28:32 +0900

    feat : 로그인화면 키보드처리 추가
    리턴버튼 클릭시 키보드 내림처리
    키보드 바깓영역 클릭시 키보드 내림처리

--- Commuting_certification/dev/ios/Automated Payroll_ team1/Automated Payroll_ team1/Login.swift
+++ Commuting_certification/dev/ios/Automated Payroll_ team1/Automated Payroll_ team1/Login.swift
@@ -7,13 +7,46 @@
 
 import UIKit
 
-class Login: UIViewController {
+class Login: UIViewController{
 
+    @IBOutlet var idText: UITextField!
+    @IBOutlet var pwText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationController?.navigationBar.barTintColor = .systemTeal
+        idText.delegate = self
+        pwText.delegate = self
     }
-
-
+    @IBAction func clickSubmit(_ sender: UIButton) {
+        
+        
+        // 아이디 비번이 맞을경우
+        guard let nextVC = self.storyboard?.instantiateViewController(identifier: "TakePhoto") as? TakePhoto else {return}
+        nextVC.id = idText.text ?? ""
+        self.navigationController?.pushViewController(nextVC, animated: true)
+        
+        
+        // 틀릴경우
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    // 화면 클릭시 키보드 내림처리
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
+
+// 키보드 return 클릭시 키보드 내림 처리
+extension Login: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        idText.resignFirstResponder()
+        pwText.resignFirstResponder()
+        return true
+    }
+}
+
 
