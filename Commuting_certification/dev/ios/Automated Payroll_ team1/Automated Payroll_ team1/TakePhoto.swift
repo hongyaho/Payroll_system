cef7c9083f514dcd5c3ba9addc410c8890cfeaa7
Joon <kjo11300@naver.com>
2021-05-29 01:33:42 +0900

    feat : 사진촬영화면 구현
    기존에 좌표화면에 있던 사진촬영기능 사진촬영 화면 별도 생성 및 이동
    사진 위쪽에 좌표값 표출

--- Commuting_certification/dev/ios/Automated Payroll_ team1/Automated Payroll_ team1/TakePhoto.swift
+++ Commuting_certification/dev/ios/Automated Payroll_ team1/Automated Payroll_ team1/TakePhoto.swift
@@ -7,23 +7,39 @@
 
 import UIKit
 
-class TakePhoto: UIViewController {
+class TakePhoto: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var id = ""
 
+    @IBOutlet var idLbl: UILabel!
+    @IBOutlet var cameraImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let camera = UIImagePickerController()
+        camera.allowsEditing = false
+        camera.sourceType = .camera
+        camera.cameraDevice = .rear
+        camera.cameraCaptureMode = .photo
+        camera.delegate = self
+        present(camera, animated: true, completion: nil)
+        idLbl.text = id
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func clickNextBtn(_ sender: UIButton) {
+        guard let nextVC = self.storyboard?.instantiateViewController(identifier: "TakeLocation") as? TakeLocation else {return}
+        nextVC.id = id
+        self.navigationController?.pushViewController(nextVC, animated: true)
     }
-    */
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            cameraImage.image = image
+        }
+        self.dismiss(animated: true, completion: nil)
+
+    }
+ 
 
 }
+
