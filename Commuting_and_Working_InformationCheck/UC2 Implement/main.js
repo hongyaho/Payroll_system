0dd03b9db05983134daaf4b305f9e6282ab84ca3
Hyo-jung Lee <leeexpert@cau.ac.kr>
2021-05-30 15:26:28 +0900

    예시 사진, GPS마커 데이터 추가, 스크린샷 추가

--- Commuting_and_Working_InformationCheck/UC2 Implement/main.js
+++ Commuting_and_Working_InformationCheck/UC2 Implement/main.js
@@ -1,28 +1,120 @@
 "use strict";
-var Database = [{
-    ID: "ID1",
-    Date: "2021-05-01",
-    img: "./img/test.png"
-}, {
-    ID: "ID2",
-    Date: "2021-05-02",
-    img: "./img/test2.png"
-}, {
-    ID: "ID3",
-    Date: "2021-05-03",
-    img: "./img/test3.png"
-}]
-
-var input = {
-    inputID: 0,
-    dateInfo: 0
+// 예시 데이터 ID1, 2, 3의 ID, 날짜, 촬영사진, GPS마커
+const dataExample = [{
+    inputID: "ID1",
+    dateInfo: "2021-05-01",
+    img: "./img/test.png",
+    LatLng: [{
+            lat: 37.504486,
+            lng: 126.955123
+        },
+        {
+            lat: 37.454897,
+            lng: 126.900450
+        },
+        {
+            lat: 37.512495,
+            lng: 126.999457
+        },
+        {
+            lat: 37.576963,
+            lng: 126.957264
+        },
+        {
+            lat: 37.557390,
+            lng: 126.947746
+        },
+        {
+            lat: 37.519212,
+            lng: 126.890976
+        },
+        {
+            lat: 37.527098,
+            lng: 126.930379
+        }
+    ]
+},
+{
+    inputID: "ID2",
+    dateInfo: "2021-05-02",
+    img: "./img/test2.png",
+    LatLng: [{
+            lat: 37.504486,
+            lng: 126.955123
+        },
+        {
+            lat: 37.454897,
+            lng: 126.900450
+        },
+        {
+            lat: 37.512495,
+            lng: 126.999457
+        },
+        {
+            lat: 37.576963,
+            lng: 126.957264
+        },
+        {
+            lat: 37.557390,
+            lng: 126.947746
+        },
+        {
+            lat: 37.519212,
+            lng: 126.890976
+        },
+        {
+            lat: 37.527098,
+            lng: 126.930379
+        }
+    ]
+},
+{
+    inputID: "ID3",
+    dateInfo: "2021-05-03",
+    img: "./img/test3.png",
+    LatLng: [{
+            lat: 37.504486,
+            lng: 126.955123
+        },
+        {
+            lat: 37.454897,
+            lng: 126.900450
+        },
+        {
+            lat: 37.512495,
+            lng: 126.999457
+        },
+        {
+            lat: 37.576963,
+            lng: 126.957264
+        },
+        {
+            lat: 37.557390,
+            lng: 126.947746
+        },
+        {
+            lat: 37.519212,
+            lng: 126.890976
+        },
+        {
+            lat: 37.527098,
+            lng: 126.930379
+        }
+    ]
 }
 
-var message = "선택한 날짜에 데이터가 없습니다."
+]
+
+// DB에서 조회할 입력 데이터를 가정
+var input=
+    {
+        inputID: "ID1",
+        dateInfo: "2021-05-01"
+}
 
 class Alert {
-    static prompt(m) {
-        alert(m)
+    static prompt() {
+        alert("선택한 날짜에 데이터가 없습니다.")
     }
 }
 
@@ -34,10 +126,8 @@ class pageMaker {
 
 class DatabaseConnection {
     static isExist(dateInfo, inputID) {
-        console.log(dateInfo, inputID)
-        for (var i=0; i < Database.length; i++) {
-            console.log(Database[i])
-            if ((dateInfo === Database[i].Date) && (inputID === Database[i].ID))
+        for (var i=0; i < dataExample.length; i++) {
+            if ((dateInfo === dataExample[i].dateInfo) && (inputID === dataExample[i].inputID))
                 return true
             else return false
         }
@@ -49,19 +139,21 @@ class Controller {
     static requestMakePage() {
         if (this.isAvailableID && DatabaseConnection.isExist(input.dateInfo, input.inputID))
             pageMaker.showResultPage()
-        else this.alert(message)
+        else this.alert()
     }
-    static alert(m) {
-        Alert.prompt(m)
+    static alert() {
+        Alert.prompt()
     }
 }
 
+//UC1에서 입력한 ID가 유효한지 확인 받았음을 가정 하는 함수
 function goNextfromUC1() {
     alert('UC1에서 유효한 식별번호임이 확인 되었을 시 이동됩니다.');
     Controller.isAvailableID = true;
     getInput()
 }
 
+// 사용자의 입력 데이터를 저장
 function getInput() {
     if (Controller.isAvailableID) {
         input.inputID = document.getElementById("inputID").value;
--- Commuting_and_Working_InformationCheck/UC2 Implement/map.js
+++ Commuting_and_Working_InformationCheck/UC2 Implement/map.js
@@ -3,7 +3,39 @@ var options = {
     center: new kakao.maps.LatLng(37.504403, 126.955348),
     level: 5
 };
+// 예시 GPS 마커
+var examplePositions=[
+    {
+        lat:37.504486,
+        lng:126.955123
+    },
+    {
+        lat:37.454897,
+        lng:126.900450
+    },
+    {
+        lat:37.512495,
+        lng:126.999457
+    },
+    {
+        lat:37.576963,
+        lng:126.957264
+    },
+    {
+        lat:37.557390,
+        lng:126.947746
+    },
+    {
+        lat:37.519212,
+        lng:126.890976
+    },
+    {
+        lat:37.527098,
+        lng:126.930379
+    }
+]
 
+// 맵 컨트롤 도구 추가
 var map = new kakao.maps.Map(container, options);
 var mapTypeControl = new kakao.maps.MapTypeControl();
 map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
@@ -11,6 +43,7 @@ map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
 var zoomControl = new kakao.maps.ZoomControl();
 map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 
+// 접속위치 추가
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
         var lat = position.coords.latitude,
@@ -45,11 +78,12 @@ function displayMarker(locPosition, message) {
     map.setCenter(locPosition);
 }
 
-var points = [
-    new kakao.maps.LatLng(33.452278, 126.567803),
-    new kakao.maps.LatLng(33.452671, 126.574792),
-    new kakao.maps.LatLng(33.451744, 126.572441)
-];
+// 예시 데이터의 좌표값을 points에 추가
+var points = [];
+for (var i=0; i<examplePositions.length;i++){
+    points.push(new kakao.maps.LatLng(examplePositions[i].lat,examplePositions[i].lng));
+}
+
 
 // 지도를 재설정할 범위정보를 가지고 있을 LatLngBounds 객체를 생성합니다
 var bounds = new kakao.maps.LatLngBounds();
