ef310e9092666ae507946ea2f0b947c6e91c524b
Joon <kjo11300@naver.com>
2021-05-31 18:59:31 +0900

    feat : 서버 전송관련 코드
    위경도 데이터등의 출근인증 데이터 서버 전송 관련 코드

--- Commuting_certification/dev/ios/Automated Payroll_ team1/Automated Payroll_ team1/Login.swift
+++ Commuting_certification/dev/ios/Automated Payroll_ team1/Automated Payroll_ team1/Login.swift
@@ -6,9 +6,14 @@
 //
 
 import UIKit
+import Firebase
+import Alamofire
+import SwiftyJSON
+
+let ad = UIApplication.shared.delegate as! AppDelegate
 
 class Login: UIViewController{
-
+    
     @IBOutlet var idText: UITextField!
     @IBOutlet var pwText: UITextField!
     override func viewDidLoad() {
@@ -17,17 +22,83 @@ class Login: UIViewController{
         idText.delegate = self
         pwText.delegate = self
     }
-    @IBAction func clickSubmit(_ sender: UIButton) {
+    
+    // firebase 코드
+//    @IBAction func clickSignUp(_ sender: UIButton) {
+//        let email = idText.text ?? ""
+//        let password = pwText.text ?? ""
+//
+//        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+//            if (error != nil) {
+//                guard let error = error else {return}
+//                let alert = UIAlertController(title: "Error", message: "\(error.localizedDescription)", preferredStyle: .alert)
+//                let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+//                alert.addAction(okAction)
+//                self.present(alert, animated: true, completion: nil)
+//                return
+//            }
+//
+//            guard let nextVC = self.storyboard?.instantiateViewController(identifier: "TakePhoto") as? TakePhoto else {return}
+//            nextVC.id = self.idText.text ?? ""
+//            self.navigationController?.pushViewController(nextVC, animated: true)
+//
+//        }
+//    }
+    
+    
+    @IBAction func clickLogin(_ sender: UIButton) {
         
+        // ** Firebase 사용코드
+//        let email = idText.text ?? ""
+//        let password = pwText.text ?? ""
+//        Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+//          guard let strongSelf = self else { return }
+//            if (error != nil) {
+//                let alert = UIAlertController(title: "Login Error", message: "Check id and password", preferredStyle: .alert)
+//                let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+//                alert.addAction(okAction)
+//                self!.present(alert, animated: true, completion: nil)
+//                return
+//            }
+//
+//            // 아이디 비번이 맞을경우
+//            guard let nextVC = self?.storyboard?.instantiateViewController(identifier: "TakePhoto") as? TakePhoto else {return}
+//            nextVC.id = self?.idText.text ?? ""
+//            self?.navigationController?.pushViewController(nextVC, animated: true)
+//
+//        }
         
-        // 아이디 비번이 맞을경우
-        guard let nextVC = self.storyboard?.instantiateViewController(identifier: "TakePhoto") as? TakePhoto else {return}
-        nextVC.id = idText.text ?? ""
-        self.navigationController?.pushViewController(nextVC, animated: true)
+        // 로그인 
+        let URL = "http://3.36.39.242/api/v1/users/login/"
+        let PARAM: [String: String] = [
+            "username" : idText.text ?? "",
+            "password" : pwText.text ?? ""
+        ]
         
+        let alamo = AF.request(URL, method: .post, parameters: PARAM).validate(statusCode: 200..<300)
         
-        // 틀릴경우
-        
+        alamo.responseString { response in
+            switch response.result
+            {
+            case .success(let value):
+                print("login success")
+                let json = JSON(value)
+                print(json)
+                print(json.stringValue)
+                guard let nextVC = self.storyboard?.instantiateViewController(identifier: "TakePhoto") as? TakePhoto else {return}
+                nextVC.id = self.idText.text ?? ""
+                self.idText.text = ""
+                self.pwText.text = ""
+                self.navigationController?.pushViewController(nextVC, animated: true)
+                
+            case .failure(let error):
+                print("error: \(error.errorDescription)")
+                let alert = UIAlertController(title: "Login Error", message: "Check id and password", preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alert.addAction(okAction)
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
--- Commuting_certification/dev/ios/Automated Payroll_ team1/Automated Payroll_ team1/Main.swift
+++ Commuting_certification/dev/ios/Automated Payroll_ team1/Automated Payroll_ team1/Main.swift
@@ -8,6 +8,7 @@
 import UIKit
 import CoreLocation
 import Firebase
+import Alamofire
 
 class Main: UIViewController, CLLocationManagerDelegate{
 
@@ -27,34 +28,72 @@ class Main: UIViewController, CLLocationManagerDelegate{
         latLbl.text = lat
         longLBl.text = long
         
-//        locationManager = CLLocationManager()
-//        locationManager.delegate = self
-//
-//        // 사용자에게 위치 허용 alert 띄우기
-//        locationManager.requestWhenInUseAuthorization()
-//        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-//        locationManager.startUpdatingLocation()
-//
-//        // 위경도 가져오기
-//        let coor = locationManager.location?.coordinate
-//        let latitude = coor?.latitude
-//        let longitude = coor?.longitude
-//
-//        if let latitude = latitude, let longitude = longitude {
-//            latLbl.text = String(format:"%.3f", latitude)
-//            longLBl.text = String(format:"%.3f", longitude)
-//        }
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+
+        // 사용자에게 위치 허용 alert 띄우기
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+
+        // 위경도 가져오기
+        let coor = locationManager.location?.coordinate
+        let latitude = coor?.latitude
+        let longitude = coor?.longitude
+
+        if let latitude = latitude, let longitude = longitude {
+            latLbl.text = String(format:"%.3f", latitude)
+            longLBl.text = String(format:"%.3f", longitude)
+        }
         
         idLBl.text = id
         
     }
     @IBAction func clickLogout(_ sender: UIButton) {
-        let firebaseAuth = Auth.auth()
-        do {
-            try firebaseAuth.signOut()
-        } catch let signOutError as NSError {
-            print ("Error signing out: %@", signOutError)
+        
+        // ** Firebase 코드
+//        let firebaseAuth = Auth.auth()
+//        do {
+//            try firebaseAuth.signOut()
+//        } catch let signOutError as NSError {
+//            print ("Error signing out: %@", signOutError)
+//        }
+        
+        // 퇴근인증
+        let URL = "http://3.36.39.242/api/v1/attendances/1/"
+        let header: HTTPHeaders = [
+            "Authorization" : "Bearer " + ad.token]
+        
+        let PARAM: [String: String] = [
+            "end_latitude" : lat ,
+            "end_longitude" : long
+        ]
+        
+        let alamo = AF.request(URL, method: .put, parameters: PARAM, headers: header).validate(statusCode: 200..<300)
+        
+        
+        alamo.responseString(emptyResponseCodes: [200,204,205]) { response in
+            print(PARAM)
+            switch response.result
+            {
+            case .success(let value):
+                print("put success")
+                guard let nextVC = self.storyboard?.instantiateViewController(identifier: "Main") as? Main else {return}
+                nextVC.lat = self.lat
+                nextVC.long = self.long
+                nextVC.id = self.id
+                self.navigationController?.pushViewController(nextVC, animated: true)
+                
+            case .failure(let error):
+                print("error: \(error.errorDescription!)")
+                let alert = UIAlertController(title: "Error", message: error.errorDescription, preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alert.addAction(okAction)
+                self.present(alert, animated: true, completion: nil)
+            }
         }
+        
+        
         self.navigationController?.popToRootViewController(animated: true)
         
         
